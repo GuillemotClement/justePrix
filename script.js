dea663894157f76234ffd6f76d944ffea99ec274
GuillemotClement <guillemot.clement@protonmail.com>
2023-04-07 09:56:51 +0200

    Step 6 - Fonction vérifier

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let formulaire  = document.querySelector("#formulaire");
 error.style.display = "none";
 // Etape 3 - Générer un nombre aléatoire
 let nbAleatoire = Math.floor(Math.random() * 1001);
+let coups       = 0;
+let nbChoisi;
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
  input.addEventListener('keyup', () => {
-    if(isNaN( input.value){
+    if(isNaN(input.value)){
         error.style.display = "inline";
     }
     else{
@@ -16,5 +18,16 @@ let nbAleatoire = Math.floor(Math.random() * 1001);
     }
  });
 // Etape 5 - Agir à l'envoi du formulaire
-
+formulaire.addEventListener('submit', (e)=>{
+    e.preventDefault(); 
+    if(isNaN(input.value) || input.value == ""){
+        input.style.borderColor = "red";
+    } else {
+        coups++;
+        input.style.borderColor = "silver";
+        nbChoisi = input.value;
+        input.value = '';
+        verifier(nbChoisi);
+    }
+});
 // Etape 6 - Créer la fonction vérifier
